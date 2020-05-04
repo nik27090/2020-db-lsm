e8df1b3fd0e5f60cf8b140e319f60928f9771547
nik27090 <nik27090@mail.ru>
2020-05-04 22:37:55 +0300

    codeClimate fix

--- src/main/java/ru/mail/polis/nik27090/DAOImpl.java
+++ src/main/java/ru/mail/polis/nik27090/DAOImpl.java
@@ -2,9 +2,6 @@ package ru.mail.polis.nik27090;
 
 import com.google.common.collect.Iterators;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import ru.mail.polis.Client;
 import ru.mail.polis.DAO;
 import ru.mail.polis.Iters;
 import ru.mail.polis.Record;
